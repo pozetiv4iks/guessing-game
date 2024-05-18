b2d8734556f3c389593676a3d6da45cc92a50857
pozetiv4iks <stepan.ewqazsx@gmail.com>
2024-05-18 22:07:27 +0300

    update: guessing-game.js

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,21 +1,27 @@
 class GuessingGame {
-    constructor() {}
-
+    constructor() {
+      this.min;
+      this.max;
+      this.resultsGame;
+    }
+  
     setRange(min, max) {
-
+      this.min = min;
+      this.max = max;
     }
-
+  
     guess() {
-
+      this.resultsGame = Math.round((this.max - this.min) / 2) + this.min;
+      return this.resultsGame;
     }
-
+  
     lower() {
-
+      this.max = this.resultsGame;
     }
-
+  
     greater() {
-
+      this.min = this.resultsGame;
     }
-}
-
-module.exports = GuessingGame;
+  }
+  
+  module.exports = GuessingGame;
